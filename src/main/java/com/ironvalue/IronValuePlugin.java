882f9f73eb182813061cc057ad0283a7873a23a1
Josh Hawkins <hawkins@users.noreply.github.com>
2022-06-01 18:10:55 -0400

    Add first pass of overlay copying the item prices overlay

--- src/main/java/com/ironvalue/IronValuePlugin.java
+++ src/main/java/com/ironvalue/IronValuePlugin.java
@@ -1,5 +1,6 @@
 package com.ironvalue;
 
+import com.ironvalue.IronValueOverlay;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
 @Slf4j
 @PluginDescriptor(
@@ -19,35 +21,30 @@ import net.runelite.client.plugins.PluginDescriptor;
 public class IronValuePlugin extends Plugin
 {
 	@Inject
-	private Client client;
+	private OverlayManager overlayManager;
 
 	@Inject
-	private IronValueConfig config;
+	private IronValueOverlay overlay;
+
+	@Provides
+	IronValueConfig getConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(IronValueConfig.class);
+	}
 
 	@Override
 	protected void startUp() throws Exception
 	{
+		log.info("Iron Value starting...");
+		overlayManager.add(overlay);
 		log.info("Iron Value started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
+		log.info("Iron Value stopping...");
+		overlayManager.remove(overlay);
 		log.info("Iron Value stopped!");
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
-
-	@Provides
-	IronValueConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(IronValueConfig.class);
 	}
 }
