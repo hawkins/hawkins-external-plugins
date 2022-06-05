78321a63671727f296c19d931fc11293bd716998
Josh Hawkins <hawkins@users.noreply.github.com>
2022-06-05 11:27:31 -0400

    Hardcode black square waypoint at startup

--- src/main/java/com/waypoints/WaypointsPlugin.java
+++ src/main/java/com/waypoints/WaypointsPlugin.java
@@ -6,11 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.worldmap.WorldMapPoint;
+import net.runelite.client.ui.overlay.worldmap.WorldMapPointManager;
+
+import java.awt.image.BufferedImage;
 
 @Slf4j
 @PluginDescriptor(
@@ -24,16 +29,24 @@ public class WaypointsPlugin extends Plugin
 	@Inject
 	private WaypointsConfig config;
 
+	@Inject
+	private WorldMapPointManager worldMapPointManager;
+
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		log.info("Waypoints starting...");
+		WorldPoint worldPoint = new WorldPoint(1765, 3847, 0);
+		BufferedImage bufferedImage = new BufferedImage(32, 32, BufferedImage.TYPE_INT_RGB);
+		WorldMapPoint worldMapPoint = new WorldMapPoint(worldPoint, bufferedImage);
+		worldMapPointManager.add(worldMapPoint);
+		log.info("Waypoints started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		log.info("Waypoints stopped!");
 	}
 
 	@Subscribe
